25441778d00ebbcea90e1a3b573264f47f376d9d
langlangpig <2655448668@qq.com>
2024-05-20 11:46:06 +0800

    chapter04

---
+++ chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿static string CardinalToOrdinal(int number)
+{
+    int lastTwoDigits = number % 100;
+    switch(lastTwoDigits)
+    {
+        case 11:
+        case 12:
+        case 13:
+            return $"{number}th";
+        default:
+            int lastDigit=number%10;
+
+            string suffix=lastDigit switch
+            {   
+                1=>"st",
+                2=>"nd",
+                3=>"rd",
+                _=>"th"
+            }; 
+            return $"{number:N0}{suffix}";        
+                }
+}
